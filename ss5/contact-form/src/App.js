3a9a29629769db3997b4fbd9b4c5f3135ca56ca2
phongton <phongton219@gmail.com>
2024-08-22 08:48:38 +0700

    upbai5

---
+++ ss5/contact-form/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import FormContact from "./component/FormContact";
+
+function App() {
+  return (
+   <FormContact></FormContact>
+  );
+}
+
+export default App;
