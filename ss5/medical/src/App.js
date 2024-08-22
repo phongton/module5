22f9c10dd3b4f8c81391fb44cb91e082406e4740
phongton <phongton219@gmail.com>
2024-08-22 09:01:17 +0700

    up bai tap5

---
+++ ss5/medical/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Medical from "./component/Medical";
+
+function App() {
+  return (
+   <Medical></Medical>
+  );
+}
+
+export default App;
